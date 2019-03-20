8a39e82219dce91b0fc4cc44e401b8fc8637193c
andreasbm <andmehlsen@gmail.com>
2019-03-20 14:12:44 +0100

    Added cleanup to karma.

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,13 @@
-const {defaultResolvePlugins, defaultKarmaConfig} = require("@appnest/web-config");
+const {defaultResolvePlugins, defaultKarmaConfig, clean} = require("@appnest/web-config");
 const path = require("path");
 
 module.exports = (config) => {
 	config.set({
 		...defaultKarmaConfig({
-			rollupPlugins: defaultResolvePlugins()
+			rollupPlugins: [
+				clean({targets: ["dist"]}),
+				...defaultResolvePlugins()
+			]
 		}),
 		basePath: "src",
 		logLevel: config.LOG_INFO
